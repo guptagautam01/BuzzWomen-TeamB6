4f9201ccb825d277e4e2e677b076897c7f8c6cc1
vishal <vvish4me@gmail.com>
2022-06-14 12:33:15 +0530

    adding phone gateway
    Signed-off-by: vishal <vvish4me@gmail.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,71 @@
-import React from 'react';
-import { AppBar,Button,Toolbar,Typography } from '@mui/material';
-
-function Navbar(){
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Dialog,
+  DialogTitle,
+  Button,
+  List,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+function SimpleDialog(props) {
+  const { handleClose, open } = props;
+  const copyToClipBoard=(e)=>{
+    const cb=navigator.clipboard
+    cb.writeText(e.target.innerText)
+  }
   return (
-        <AppBar position='static'>
+    <Dialog open={open}>
+      <DialogTitle style={{ display: "flex" }}>
+        <div style={{ fontFamily: "Poppins" ,margin:'0px 10px'}}>Phone Gateway</div>
+        <Button onClick={handleClose} style={{ marginLeft: "40px" }}>
+          <CloseIcon />
+        </Button>
+      </DialogTitle>
+      <List sx={{ pt: 0 ,textAlign:'center',fontSize:'large'}}>
+        Contact us : <span  onClick={copyToClipBoard} style={{cursor:'pointer',color:'blue'}}>+1 860-317-0564</span>
+      </List>
+    </Dialog>
+  );
+}
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <SimpleDialog open={open} handleClose={handleClose} />
+      <AppBar position="static">
         <Toolbar>
-        <Typography variant='h6'  style={{ color: 'white' }}  flexGrow={1} fontFamily={'Poppins'}><b>Sakhi</b></Typography>
-        
+          <Typography
+            variant="h6"
+            style={{ color: "white" }}
+            flexGrow={1}
+            fontFamily={"Poppins"}
+          >
+            <b>Sakhi</b>
+          </Typography>
+          <Typography
+            variant="h6"
+            style={{ color: "white", textAlign: "end", cursor: "pointer" }}
+            flexGrow={1}
+            fontFamily={"Poppins"}
+            onClick={handleClickOpen}
+          >
+            Phone Gateway
+          </Typography>
         </Toolbar>
-        </AppBar>
-  )
+      </AppBar>
+    </>
+  );
 }
 
 export default Navbar;
